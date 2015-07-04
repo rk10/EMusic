bd8fc78282a6e2cd453bea4e116713ca76539156
Lempikot <lempikot.kottar@gmail.com>
2015-07-04 18:51:32 +0300

    Deleting of garbage

--- EMusic/Models/Player.cs
+++ EMusic/Models/Player.cs
@@ -44,7 +44,7 @@ namespace EMusic.Models
         {
             bool res =
                 (_player.controls.currentPosition >= _player.controls.currentItem.duration &&
-                _player.controls.currentItem.duration > 0) ||
+                _player.controls.currentItem.duration > 0) || 
                 (_saveTrack == Track && _player.controls.currentPosition <= 0 && _savePos > 0 && _isChangeTrack);
 
             _saveTrack = _track;
